e7e0517e21f8f35f6360bbec63d347f702592dbc
Wonhyeok <dnjgur0629@yonsei.ac.kr>
2022-04-04 09:35:41 +0900

    ---

---
+++ frequently-used-feature/recoil-todo-list/src/App.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import {atom, selector, useRecoilValue} from "recoil";
+import axios from "axios";
+
+const todoIdState = atom({
+  key: 'todoIdState',
+  default: 1,
+});
+
+const todoItemQuery = selector({
+  key: 'todoIdState',
+  get: async ({get}) => {
+    const id = get(todoIdState);
+
+    const response = await axios.get(`https://jsonplaceholder.typicode.com/todos/${id}`)
+
+    return response.data;
+  }
+})
+
+function App() {
+  const data = useRecoilValue(todoItemQuery);
+
+  return (
+      <div>
+        {data.title}
+      </div>
+  );
+}
+
+export default App;
