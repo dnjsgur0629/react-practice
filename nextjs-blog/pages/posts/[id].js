95a69a9760c9103369572008ea9d3037bea448fc
Wonhyeok <dnjgur0629@yonsei.ac.kr>
2022-04-05 09:41:01 +0900

    resolve some bugs

--- nextjs-blog/pages/posts/[id].js
+++ nextjs-blog/pages/posts/[id].js
@@ -1,9 +1,10 @@
 import Layout from '../../components/layout'
-import {getPostData} from "../../lib/posts";
+import {getAllPostIds, getPostData} from "../../lib/posts";
 import Head from 'next/head'
 import utilStyles from '../../styles/utils.module.css'
 import {useRouter} from "next/router";
 import {useEffect} from "react";
+import Date from "../../components/date";
 
 //동적 라우팅을 위해 paths를 받아와서 getStaticPaths에서 리턴
 export async function getStaticPaths() {
