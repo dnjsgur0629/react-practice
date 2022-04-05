19472555016913f68dffef9411c5f04863cc2e7c
Wonhyeok <dnjgur0629@yonsei.ac.kr>
2022-04-05 18:05:00 +0900

    Next.js 심화

---
+++ nextjs-blog/__test__/index.test.js
@@ -0,0 +1,16 @@
+// __tests__/index.test.jsx
+
+import {render, screen} from '@testing-library/react'
+import FirstPost from "../pages/posts/first-post";
+
+describe('FirstPost', () => {
+  it('renders a heading', () => {
+    render(<FirstPost/>);
+
+    const heading = screen.getByRole('heading', {
+      name: /First Post/i,
+    })
+
+    expect(heading).toBeInTheDocument()
+  })
+})
